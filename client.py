fe730634663a74a93bbdbdb80e6c35ebeb0d3363
ricky mamon <rickymamon95@gmail.com>
2024-12-18 00:35:39 +0800

    add job

--- client.py
+++ client.py
@@ -7,3 +7,16 @@ def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
+
+def test_add_job(client):
+    response = client.post('/add_job',
+        json={
+            "id": "101",
+            "fullname": "Ricky mamon",
+            "job_title": "Software Engineer",
+            "description": "Develop and maintain software applications.",
+            "department": "IT",
+            "address": "Palawan"
+        })
+    assert response.status_code == 404
