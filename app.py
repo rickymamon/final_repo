2b479b7222130e8acba067dccec8b0f0a7228469
ricky mamon <rickymamon95@gmail.com>
2024-12-18 01:13:58 +0800

    used to deploy to each function so call

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt, verify_jwt_in_request
 from functools import wraps
+
+app = Flask(__name__)
+jwt = JWTManager(app)
