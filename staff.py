e8dbbf7dae0eee5fad57ee045d9a110454656272
ricky mamon <rickymamon95@gmail.com>
2024-12-18 00:57:04 +0800

    add client

--- staff.py
+++ staff.py
@@ -8,3 +8,12 @@ def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+        
+def test_add_client(client):
+    response = client.post('/add_client', 
+        json={
+            "client_name": "",
+            "start_date": "2024-12-15",
+            "job_department": "IT"
+        })
+    assert response.status_code == 404
